e36aa41f6686951a46b197abeb3ff69e2c388b94
Qijing Li <qijing.lee@gmail.com>
2025-01-12 18:24:30 -0800

    Initial Commit

---
+++ ScanText/ScanTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScanTextApp.swift
+//  ScanText
+//
+//  Created by Qijing li on 1/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ScanTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScanTextTests/ScanTextTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScanTextTests.swift
+//  ScanTextTests
+//
+//  Created by Qijing li on 1/12/25.
+//
+
+import Testing
+@testable import ScanText
+
+struct ScanTextTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
